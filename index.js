8dbdca01cf3de26b6bb2f302fc18b47432ab52c4
dburandt <behringer.19@gmail.com>
2020-02-03 21:45:18 -0500

    add environment variable

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const express = require('express');
+require("dotenv").config();
+
+const express = require("express");
 
 const app = express();
 const log = console.log;
 const port = process.env.PORT || 8080;
+const { ENVIRONMENT } = process.env;
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Healthy v3');
+app.get("/", (req, res) => {
+  res.send(`Healthy v3. Running in ${ENVIRONMENT}`);
 });
 
-app.listen(port, () => log('Running 🤙'));
+app.listen(port, () => log(`Running on ${port} 🤙`));
